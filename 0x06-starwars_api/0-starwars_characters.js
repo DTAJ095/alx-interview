cf5a8bb2c4068393a4b7c67b1611adabd8911563
DTAJ095 <albanjaures26@gmail.com>
2024-07-10 08:45:26 -0700

    corrected

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@
  * @param {string} url - The URL to make the request to.
  * @returns {Promise} - The promise object representing the request.
  */
-function makeRequest(url) {
+function makeRequest (url) {
   const request = require('request');
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
@@ -20,26 +20,26 @@ function makeRequest(url) {
  * and prints the names of the characters in the response.
  * in order of appearance in the films.
  */
-async function main() {
-    const args = process.argv;
+async function main () {
+  const args = process.argv;
 
-    if (args.length !== 3) {
-        console.log('Usage: ./0-starwars_characters.js <film number>');
-        process.exit(1);
-    }
+  if (args.length !== 3) {
+    console.log('Usage: ./0-starwars_characters.js <film number>');
+    process.exit(1);
+  }
 
-    const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
-    const movie = await makeRequest(movieUrl);
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+  const movie = await makeRequest(movieUrl);
 
-    if (movie.characters == undefined) {
-        console.log('No characters found');
-        process.exit(1);
-    }
+  if (movie.characters === undefined) {
+    console.log('No characters found');
+    process.exit(1);
+  }
 
-    for (const characterUrl of movie.characters) {
-        const character = await makeRequest(characterUrl);
-        console.log(character.name);
-    }
+  for (const characterUrl of movie.characters) {
+    const character = await makeRequest(characterUrl);
+    console.log(character.name);
+  }
 }
 
 main();
