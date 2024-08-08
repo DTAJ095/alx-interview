e024d696babc5980b35a97f9b7a08334bb329d2a
DTAJ095 <albanjaures26@gmail.com>
2024-08-08 20:05:15 +0100

    updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,6 +2,27 @@
 """Prime Game"""
 
 
+def isWinner(x: int, nums:list) -> str:
+    """Prime Game"""
+    if x == 0 or x == 1:
+        return None
+    if x != len(nums):
+        return None
+    n = max(nums)
+    nums.sort()
+    primes = [1 for x in range(n + 1)]
+    primes[0], primes[1] = 0, 0
+    for i in range(2, len(primes)):
+        if primes[i] == 1:
+            for j in range(i * i, len(primes), i):
+                primes[j] = 0
+    primes_sum = [0]
+    for i in range(1, len(primes)):
+        primes_sum.append(primes_sum[i - 1] + primes[i])
+    if sum(primes) % 2 == 0:
+        return "Maria"
+    return "Ben"
+
 # def rm_multiples(n: int, nums: list) -> list:
 #     """Remove multiples"""
 #     for i in range(2, len(nums)):
@@ -37,28 +58,3 @@
 #     if ben > maria:
 #         return "Ben"
 #     return None
-def isWinner(x, nums):
-    """ Prime Game """
-    if not nums or x < 1:
-        return None
-    n = max(nums)
-    nums.sort()
-    m = [False for i in range(n + 1)]
-    for i in range(2, int(n ** 0.5) + 1):
-        if not m[i]:
-            for j in range(i*i, n + 1, i):
-                m[j] = True
-    m[0] = m[1] = True
-    c = 0
-    for i in range(len(m)):
-        if not m[i]:
-            c += 1
-        m[i] = c
-    p1 = 0
-    for n in nums:
-        p1 += m[n] % 2 == 1
-    if p1 * 2 == len(nums):
-        return None
-    if p1 * 2 > len(nums):
-        return "Maria"
-    return "Ben"
