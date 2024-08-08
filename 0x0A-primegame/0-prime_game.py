1b0d2dd00e2da30f2e982ba479819a0341f1c4ea
DTAJ095 <albanjaures26@gmail.com>
2024-08-08 18:02:45 +0100

    updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,12 +2,29 @@
 """Prime Game"""
 
 
+def rm_multiples(x, nums):
+    """Remove multiples"""
+    nums = sorted(nums)
+    nums = list(dict.fromkeys(nums))
+    nums = [num for num in nums if num % x != 0]
+    return nums
+
+
 def isWinner(x, nums):
     """Prime Game"""
-    if x == 0 or x == 1:
+    if not nums:
         return None
+
     if x != len(nums):
         return None
+
+    if x == 0 or x == 1:
+        return None
+
     if x % 2 == 0:
         return "Ben"
-    return "Maria"
+    nums = rm_multiples(2, nums)
+
+    if not nums:
+        return "Maria"
+    return "Ben"
