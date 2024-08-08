7bddd7c03d31bfbd1390513453685faaae3d7ad8
DTAJ095 <albanjaures26@gmail.com>
2024-08-08 18:47:05 +0100

    updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,26 +2,48 @@
 """Prime Game"""
 
 
-def rm_multipes(n: int, ls: list) -> list:
-    """Remove multiples"""
-    for i in range(2, n + 1):
-        if ls[i] == 0:
-            continue
-        for j in range(i * i, n + 1, i):
-            ls[j] = 0
-    return ls
+def isPrime(n: int) -> bool:
+    """Prime Game"""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def rm_multiple(n: int, m: int) -> list:
+    """Prime Game"""
+    multiples = []
+    for i in range(n, m + 1):
+        if isPrime(i):
+            multiples.append(i)
+    return multiples
+
 
 def isWinner(x: int, nums: list) -> str:
     """Prime Game"""
     if x == 0 or x == 1:
         return None
-    ls = [1] * (max(nums) + 1)
-    ls = rm_multipes(max(nums), ls)
-    prime = 0
-    for i in range(2, len(ls)):
-        if ls[i] == 1:
-            prime += 1
-        ls[i] = prime
-    if prime % 2 == 0:
+    if x != len(nums):
+        return None
+    
+    maria = 0
+    ben = 0
+    multiples = rm_multiple(1, max(nums))
+    for i in nums:
+        if i in multiples:
+            maria += 1
+        else:
+            ben += 1
+    if maria > ben:
         return "Maria"
-    return "Ben"
+    if ben > maria:
+        return "Ben"
+    return None
