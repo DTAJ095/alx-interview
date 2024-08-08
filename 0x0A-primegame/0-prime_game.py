f0a4e753e42e28879f4213b7b1b6ab7ab5d38773
DTAJ095 <albanjaures26@gmail.com>
2024-08-08 18:36:55 +0100

    updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,25 +2,26 @@
 """Prime Game"""
 
 
-def rm_multiples(n: int, nums: list) -> list:
+def rm_multipes(n: int, ls: list) -> list:
     """Remove multiples"""
-    for i in range(n * 2, len(nums), n):
-        nums[i] = 0
-    return nums
-
+    for i in range(2, n + 1):
+        if ls[i] == 0:
+            continue
+        for j in range(i * i, n + 1, i):
+            ls[j] = 0
+    return ls
 
 def isWinner(x: int, nums: list) -> str:
     """Prime Game"""
     if x == 0 or x == 1:
         return None
-    if x != len(nums):
-        return None
-    if len(nums) == 0:
-        return None
-    nums[1] = 0
-    for i in range(2, len(nums)):
-        if nums[i] != 0:
-            nums = rm_multiples(i, nums)
-    if nums.count(0) >= x:
+    ls = [1] * (max(nums) + 1)
+    ls = rm_multipes(max(nums), ls)
+    prime = 0
+    for i in range(2, len(ls)):
+        if ls[i] == 1:
+            prime += 1
+        ls[i] = prime
+    if prime % 2 == 0:
         return "Maria"
     return "Ben"
